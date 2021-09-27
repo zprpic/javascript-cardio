191eeadad7a6b3cc6a1105d8f97e071a8fbd9630
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-27 15:32:49 +0200

    Task #1 with for loop done

--- tasks/reverse-string.js
+++ tasks/reverse-string.js
@@ -5,10 +5,16 @@
 module.exports = reverseString = (string) => {
   /*   const stringToArray = string.split("");
   const reversedArray = stringToArray.reverse();
-  const reversedString = reversedArray.join(""); */
-
-  // const reversedString = string.split("").reverse().join("");
-  // return reversedString;
-
-  return string.split("").reverse().join("");
+  const reversedString = reversedArray.join("");
+  return reversedString;
+   */
+  /*   const reversedString = string.split("").reverse().join("");
+  return reversedString;
+  return string.split("").reverse().join(""); */
+  /*   const stringToArray = string.split("");
+  let reversedString = "";
+  for (let i = stringToArray.length - 1; i >= 0; i--) {
+    reversedString += stringToArray[i];
+  }
+  return reversedString; */
 };
