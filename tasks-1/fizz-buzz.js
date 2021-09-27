8ed8d5ee28d771297acebbc936d1a894c86a88b7
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-27 18:10:08 +0200

    FizzBuzz with better solution

--- tasks-1/fizz-buzz.js
+++ tasks-1/fizz-buzz.js
@@ -5,9 +5,17 @@
 
 module.exports = fizzBuzz = (number) => {
   for (let i = 1; i <= number; i++) {
-    if (i % 3 === 0 && i % 5 === 0) console.log(`Fizz Buzz: ${i}`);
-    else if (i % 3 === 0) console.log(`Fizz: ${i}`);
-    else if (i % 5 === 0) console.log(`Buzz: ${i}`);
-    else console.log(i);
+    // if (i % 15 === 0) {
+    //console.log(`Fizz Buzz: ${i}`)
+    //}
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`Fizz Buzz: ${i}`);
+    } else if (i % 3 === 0) {
+      console.log(`Fizz: ${i}`);
+    } else if (i % 5 === 0) {
+      console.log(`Buzz: ${i}`);
+    } else {
+      console.log(i);
+    }
   }
 };
