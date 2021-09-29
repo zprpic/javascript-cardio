28ab89235117e282ad8937b3bc1a5790192b24ec
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-29 17:31:16 +0200

    Flattening array with reduce method

--- tasks-2/flatten-array.js
+++ tasks-2/flatten-array.js
@@ -1,12 +1,15 @@
 // Take an array of arrays and flatten to a single array
 // e.g. [[1, 2], [3, 4], [5, 6], [7]] === "[1, 2, 3, 4, 5, 6, 7]"
 
-module.exports = flattenArray = (array) => {
-  const flattenedArray = [];
-  /*   array.forEach((miniArray) => {
+module.exports = flattenArray = (arrays) => {
+  /*   arrays.forEach((miniArray) => {
     miniArray.forEach((item) => {
       flattenedArray.push(item);
     });
+      return flattenedArray;
   }); */
-  return flattenedArray;
+
+  return arrays.reduce((a, b) => {
+    return a.concat(b);
+  });
 };
